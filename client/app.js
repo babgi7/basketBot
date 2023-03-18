6dcab1014d845863d096861c7775dfce9d11d1e8
Mazen Babgi <babgi7@hotmail.com>
2023-03-18 13:37:50 +0300

    FE and BE

---
+++ client/app.js
@@ -0,0 +1,187 @@
+function sendUserMessage() {
+  const userInput = document.getElementById("userInput");
+  const message = userInput.value.trim();
+  if (message !== "") {
+    displayUserMessage(message);
+    userInput.value = "";
+    getBotResponse(message);
+  }
+}
+
+function displayUserMessage(message) {
+  const chatContainer = document.getElementById("chat-container");
+  const userMessage = document.createElement("div");
+  userMessage.classList.add("chat-message", "user-message");
+  userMessage.textContent = message;
+  chatContainer.appendChild(userMessage);
+}
+
+function showApplePayButton() {
+  const applePayButton = document.querySelector(".apple-pay-btn");
+  applePayButton.style.display = "inline-block";
+}
+
+function displayBotMessage(cards) {
+  const chatContainer = document.getElementById("chat-container");
+
+  cards.forEach((card) => {
+    const cardElement = document.createElement("div");
+    cardElement.classList.add("card", "chat-message", "bot-message");
+
+    const cardImage = document.createElement("img");
+    cardImage.src = card.imgUrl;
+    cardImage.classList.add("card-img-top");
+    cardElement.appendChild(cardImage);
+
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+
+    const cardTitle = document.createElement("h5");
+    cardTitle.classList.add("card-title");
+    cardTitle.textContent = card.title;
+    cardBody.appendChild(cardTitle);
+
+    const cardPrice = document.createElement("p");
+    cardPrice.classList.add("card-text");
+    cardPrice.textContent = `Price: ${card.price}`;
+    cardBody.appendChild(cardPrice);
+
+    cardElement.appendChild(cardBody);
+
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("btn", "btn-remove", "no-box-shadow");
+    deleteButton.textContent = "X";
+    deleteButton.addEventListener("click", () => {
+      cardElement.remove();
+      updateCheckoutSummary();
+    });
+    cardElement.appendChild(deleteButton);
+
+    chatContainer.appendChild(cardElement);
+  });
+}
+
+// var raw = JSON.stringify({
+//   model: "gpt-3.5-turbo",
+//   messages: [
+//     {
+//       role: "user",
+//       content: "Hello",
+//     },
+//   ],
+// });
+
+function getBotResponse(message) {
+  const loadingMessage = displayLoadingMessage();
+  console.log("###", loadingMessage);
+  // raw = JSON.stringify({
+  //   model: "gpt-3.5-turbo",
+  //   messages: [
+  //     {
+  //       role: "user",
+  //       content: message,
+  //     },
+  //   ],
+  // });
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  console.log("##", message);
+  const raw = JSON.stringify({
+    input: message,
+  });
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+  console.log(requestOptions);
+  fetch("http://localhost:3000/api/data", requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      removeLoadingMessage(loadingMessage);
+      displayBotMessage(result.data);
+
+      updateCheckoutSummary();
+    })
+    .catch((error) => {
+      removeLoadingMessage(loadingMessage);
+      console.error("Error:", error);
+    });
+}
+
+function displayLoadingMessage() {
+  const chatContainer = document.getElementById("chat-container");
+  const loadingMessage = document.createElement("div");
+  loadingMessage.classList.add(
+    "chat-message",
+    "bot-message",
+    "loading-message"
+  );
+  loadingMessage.textContent = "Loading...";
+  chatContainer.appendChild(loadingMessage);
+
+  return loadingMessage;
+}
+
+function removeLoadingMessage(loadingMessage) {
+  loadingMessage.remove();
+}
+
+document.getElementById("userInput").addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    sendUserMessage();
+  }
+});
+
+document.getElementById("sendButton").addEventListener("click", () => {
+  sendUserMessage();
+});
+
+function getPopoverContent(totalPrice) {
+  const vatAmount = totalPrice * 0.15;
+  const grandTotal = totalPrice + vatAmount;
+
+  return `
+    <div class="popover-content-container">
+      <div class="popover-summary">
+        Total Price: $${totalPrice.toFixed(2)}<br />
+        VAT (15%): $${vatAmount.toFixed(2)}<br />
+        <hr />
+        Total Amount: $${grandTotal.toFixed(2)}<br />
+      </div>
+
+      <div>
+      <button class="apple-pay-btn fixed-bottom w-100">Apple Pay</button>
+      </div>
+    </div>
+    
+  `;
+}
+
+function updateCheckoutSummary() {
+  const checkoutButton = document.getElementById("checkoutButton");
+  const cards = document.querySelectorAll(".card");
+  let totalPrice = 0;
+
+  cards.forEach((card) => {
+    const priceText = card.querySelector(".card-text").textContent;
+    const price = parseFloat(priceText.replace("Price: ", ""));
+    totalPrice += price;
+  });
+
+  if (cards.length > 0) {
+    checkoutButton.classList.remove("d-none");
+    const popoverInstance = mdb.Popover.getInstance(checkoutButton);
+    if (popoverInstance) {
+      popoverInstance.options.content = getPopoverContent(totalPrice);
+    } else {
+      new mdb.Popover(checkoutButton, {
+        content: getPopoverContent(totalPrice),
+        html: true,
+      });
+    }
+  } else {
+    checkoutButton.classList.add("d-none");
+  }
+}
---
+++ server/index.js
@@ -0,0 +1,154 @@
+const webdriver = require("selenium-webdriver");
+const { By, Key, until } = webdriver;
+const chrome = require("selenium-webdriver/chrome");
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+require("dotenv").config();
+const { env } = process;
+const axios = require("axios");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../client")));
+
+app.post("/api/data", async (req, res) => {
+  console.log(req.body.input);
+  const txt = req.body.input;
+  let { data } = await chatGpt(txt);
+  let text = data.choices[0].message.content;
+  console.log("text:", text);
+  let val;
+  if (!Array.isArray(text)) {
+    let newText = data.choices[0].message.content;
+    newText =
+      '\
+    ```javascript\
+    const samosaIngredients = ["Potatoes", "Peas", "Onion", "Garlic", "Ginger", "Chili peppers", "Turmeric", "Coriander powder", "Garam masala", "Cumin seeds", "Mustard seeds", "Lemon juice", "Oil", "Water", "Salt", "Pastry sheets"];\
+    ```';
+    newText = newText.replace(/```/g, "");
+    const startIndex = newText.indexOf("[");
+    const endIndex = newText.lastIndexOf("]");
+    const myArray = JSON.parse(newText.substring(startIndex, endIndex + 1));
+    val = myArray;
+  }
+  console.log("match", val);
+  val = [
+    "potatoes",
+    "peas",
+    "onions",
+    "green chilies",
+    "ginger",
+    "garlic",
+    "coriander",
+    "cumin",
+    "garam masala",
+    "turmeric",
+    "salt",
+    "oil",
+    "flour",
+    "water",
+  ];
+  val.length = 2;
+  const items = await getAllData(val);
+  return res.send({ data: items });
+  // Todo call again if no array is passed
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
+
+async function getData(word) {
+  let chromeOptions = new chrome.Options();
+  chromeOptions.addArguments("--headless=new");
+  let driver = new webdriver.Builder()
+    .forBrowser(webdriver.Browser.CHROME)
+    .setChromeOptions(chromeOptions)
+    .build();
+  try {
+    await driver.get("https://www.carrefourksa.com/mafsau/en/");
+    await driver
+      .findElement(
+        By.xpath("/html/body/div/div/div[1]/div[1]/div[1]/div[3]/div/input")
+      )
+      .sendKeys(word, Key.RETURN);
+    await driver.wait(
+      until.elementLocated(
+        By.xpath(
+          "/html/body/div/div[3]/div/div[3]/div[2]/div[3]/ul/div/div[1]/div/div/div[1]/div/ul/div[2]/div[3]/div[3]/div[1]/div"
+        )
+      ),
+      30000
+    );
+
+    title = await driver
+      .findElement(
+        By.xpath(
+          "/html/body/div/div[3]/div/div[3]/div[2]/div[3]/ul/div/div[1]/div/div/div[1]/div/ul/div[2]/div[3]/div[1]/a"
+        )
+      )
+      .getText();
+    price = await driver
+      .findElement(
+        By.xpath(
+          "/html/body/div/div[3]/div/div[3]/div[2]/div[3]/ul/div/div[1]/div/div/div[1]/div/ul/div[2]/div[3]/div[3]/div[1]/div"
+        )
+      )
+      .getText();
+    image_url = await driver
+      .findElement(
+        By.xpath(
+          "/html/body/div/div[3]/div/div[3]/div[2]/div[3]/ul/div/div[1]/div/div/div[1]/div/ul/div[2]/div[1]/div/a/img"
+        )
+      )
+      .getAttribute("src");
+
+    console.log("title:", title);
+    console.log("price:", price);
+    console.log("image_url:", image_url);
+    return {
+      title,
+      price,
+      imgUrl: image_url,
+    };
+  } finally {
+    await driver.quit();
+  }
+}
+
+async function chatGpt(input) {
+  const data = {
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content: input,
+      },
+    ],
+  };
+  // console.log(data);
+  try {
+    const res = await axios({
+      method: "post",
+      url: "https://experimental.willow.vectara.io/v1/chat/completions",
+      headers: {
+        "Content-Type": "application/json",
+        "customer-id": env.CUSTOMER_ID,
+        "x-api-key": env.X_API_KEY,
+      },
+      data: {
+        ...data,
+      },
+    });
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getAllData(items) {
+  const results = await Promise.all(items.map((item) => getData(item)));
+  return results;
+}
